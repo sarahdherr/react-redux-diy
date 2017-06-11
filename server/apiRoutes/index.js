df0d7a20f6015d129528c11e2c200d7c41a5bcfb
Sarah Herr <sarah.herr@hotmail.com>
2017-06-11 12:54:17 -0400

    add(api routes): added kitten express routes

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -4,7 +4,7 @@ const router = require('express').Router()
 
 router.use('/kittens', require('./kittens')) // matches all requests to `/api/users`
       // .use('/users', require('./users')) // matches all requires to `/api/kittens`
-      .use(function(req, res, next) {
+      .use(function (req, res, next) {
         const err = new Error('Not found.')
         err.status = 404
         next(err)
--- server/apiRoutes/kittens.js
+++ server/apiRoutes/kittens.js
@@ -1,23 +1,44 @@
 // `/api/kittens`
 const router = require('express').Router()
+const Kitten = require('../../db/models').Kitten
 
 // matches GET requests to `/api/kittens`
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   // something happens here to get all kittens
+  Kitten.findAll()
+    .then(kittens => res.send(kittens))
+    .catch(next)
+})
+
+// matches GET requests to `/api/kittens/:kittenId`
+router.get('/:kittenId', function (req, res, next) {
+  // something happens here to get a specific kitten
+  Kitten.findById(req.params.kittenId)
+    .then(kitten => res.send(kitten))
+    .catch(next)
 })
 
 // matches POST requests to `/api/kittens`
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   // something happens here to create a new kitten
+  Kitten.create(req.body)
+    .then(newKitten => res.send(newKitten))
+    .catch(next)
 })
 
 // matches PUT requests to `/api/kittens/:kittenId`
-router.put('/:kittenId', function(req, res, next) {
+router.put('/:kittenId', function (req, res, next) {
   // something happens here to update a kitten w/ kittenId
+  Kitten.update(
+    req.body,
+    {returning: true, where: req.params.kittenId}
+  )
+    .then(([_, [updatedKitten]]) => res.json(updatedKitten))
+    .catch(next)
 })
 
 // matches DELETE requests to `/api/kittens/:kittenId`
-router.delete('/:kittenId', function(req, res, next) {
+router.delete('/:kittenId', function (req, res, next) {
   // something happens here to delete a kitten w/ kittenId
 })
 
