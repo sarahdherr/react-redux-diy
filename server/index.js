eca7dc5638060a86cb3b9389b76a162aeedbbeab
Sarah Herr <sarah.herr@hotmail.com>
2017-06-23 15:38:52 -0400

    added chalk to terminal console.log

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const chalk = require('chalk')
 const express = require('express')
 const app = express()
 
@@ -14,18 +15,18 @@ app.use(morgan('dev'))
 
    .use('/api', require('./apiRoutes')) // matches all requests to /api
 
-   .get('*', function(req, res) {
+   .get('*', function (req, res) {
      res.sendFile(path.join(__dirname, '../public/index.html'))
    })
 
-   .use(function(err, req, res, next) {
+   .use(function (err, req, res, next) {
      console.error(err)
      console.error(err.stack)
      res.status(err.status || 500).send(err.message || 'Internal server error.')
    })
 
-   .listen(3000, function() {
-     console.log('Knock, knock')
-     console.log('Who\'s there?')
-     console.log('Your server, listening on port 3000')
+   .listen(3000, function () {
+     console.log(chalk.dim.magenta('Knock, knock'))
+     console.log(chalk.dim.cyan('Who\'s there?'))
+     console.log(chalk.dim.magenta('Your server,'), chalk.magenta('listening on port 3000'))
    })
