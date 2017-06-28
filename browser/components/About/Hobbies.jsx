30717a7022c8c827189f05473fc6a433d887f4f8
Sarah Herr <sarah.herr@hotmail.com>
2017-06-28 11:47:45 -0400

    update(summary): added my name

--- browser/components/About/Hobbies.jsx
+++ browser/components/About/Hobbies.jsx
@@ -9,28 +9,28 @@ export default (props) =>
           <img src='http://inweekly.net/wordpress/wp-content/uploads/2014/03/wine01.jpg' />
         </div>
         <h4>WINE</h4>
-        <p>I like wine...</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu lectus lacus. Donec condimentum hendrerit neque non eleifend. Phasellus sagittis tellus finibus nibh tincidunt, a maximus nulla faucibus. Phasellus feugiat, elit eget convallis sagittis, lacus.</p>
       </div>
       <div className='col-sm-6 col-md-3 col-lg-3'>
         <div className='hobby-img'>
           <img src='http://www.fivepillarsyoga.com/wp-content/uploads/2017/01/c12fbedba2d234e04f308c23fd73a131.jpg' />
         </div>
         <h4>YOGA</h4>
-        <p>Yoga is fun...</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum, orci vel tincidunt eleifend, est felis lobortis ipsum, nec bibendum neque mauris eget felis. Phasellus luctus posuere arcu, id rutrum augue. Morbi blandit enim tellus.</p>
       </div>
       <div className='col-sm-6 col-md-3 col-lg-3'>
         <div className='hobby-img'>
           <img src='http://static.boredpanda.com/blog/wp-content/uploads/2016/04/beautiful-fluffy-cat-british-longhair-2.jpg' />
         </div>
         <h4>CATS</h4>
-        <p>Cats are the best...</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus, magna a molestie sodales, metus tortor consequat erat, eu sodales nunc massa vitae tellus. Nulla finibus metus et eros efficitur commodo quis a arcu. Donec.</p>
       </div>
       <div className='col-sm-6 col-md-3 col-lg-3'>
         <div className='hobby-img'>
           <img src='https://openclipart.org/image/2400px/svg_to_png/217532/1425663956-outline.png' />
         </div>
         <h4>SOMETHING</h4>
-        <p>Find a better hobby...</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent malesuada pretium tellus sed semper. Praesent porttitor mauris eget magna euismod, in convallis massa vehicula. Donec in nulla eros. Nam ultricies ullamcorper vestibulum. In lacinia dui.</p>
       </div>
     </div>
   </div>
